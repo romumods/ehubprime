0486cba02247305f09d2375c262f8d1f8ced8f25
Kyle6012 <102649537+Kyle6012@users.noreply.github.com>
2023-12-29 16:59:07 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,22 +2,22 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber =  "919931122319"
-global.ownername = "Ayush Pandey"
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber =  "254791811982"
+global.ownername = "Bealth-Wave Company"
+global.ytname = "YT: Bealth.inc"
+global.socialm = "GitHub: Kyle6012"
+global.location = "Kenyan"
 
-global.ownername = 'Ayush' //owner name
+global.ownername = 'Kyle Meshack' //owner name
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'Bealth-Wave Company_✰'
 global.packname = 'Sticker By'
 global.author = 'Maria Bot'
 //console view/theme
 global.themeemoji = '🧩'
-global.wm = "Ayush botz inc."
+global.wm = "Bealth Guy."
 
 //theme link
 global.link = 'https://chat.whatsapp.com/Jllsl2OaQNoBjepxzuVsZM'
@@ -27,12 +27,12 @@ global.prefa = ['.']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
-global.autoTyping = false //auto typing
+global.autoTyping = true //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
-global.autoread_status = false //auto view status/story
+global.autoread_status = true //auto view status/story
 
 
 
